cb83bc7975fcb206731c8f684d4582655cedc940
alice42 <kadoulys@hotmail.com>
2019-01-06 17:57:27 +0100

    StateInReducers

--- app/src/reducers/user/userReducer.js
+++ app/src/reducers/user/userReducer.js
@@ -11,25 +11,28 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case types.LOGIN.REQUEST:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: true,
         isAuthenticated: false
-      });
+      };
     case types.LOGIN.SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         isAuthenticated: true,
         token: action.token,
         failure: false,
         user: action.user
-      });
+      };
     case types.LOGIN.FAILURE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         isAuthenticated: false,
         failure: true,
         errorMessage: action.err
-      });
+      };
     case types.LOGOUT:
       return initialState;
     default:
