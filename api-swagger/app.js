7d3eb937e71eab303de5d73eb5ed4f49fff3f4f5
franklin <ftabarie42@gmail.com>
2019-09-05 12:08:46 +0200

    feat(api): add validation errors

--- api-swagger/app.js
+++ api-swagger/app.js
@@ -1,9 +1,10 @@
 "use strict";
+require("dotenv").config({ path: "../.env" });
 
-var SwaggerExpress = require("swagger-express-mw");
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: "../.env" });
+const SwaggerExpress = require("swagger-express-mw");
+
 const logger = require("morgan");
 const firebase = require("firebase");
 const sendmail = require("sendmail")({
@@ -15,6 +16,8 @@ const sendmail = require("sendmail")({
   },
   silent: true
 });
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 // https://firebase.google.com/docs/database/web/read-and-write?authuser=0
 // https://console.firebase.google.com/project/musicroom-c656a/database/musicroom-c656a/data/
@@ -28,35 +31,44 @@ const firebaseConfig = {
   messagingSenderId: "1004678495915"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-module.exports = app; // for testing
-
-const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./api/swagger/swagger.yaml");
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use((_, res, next) => {
-  res.database = database;
-  res.mail = sendmail;
-  return next();
-});
-
-var config = {
+const swaggerConfig = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
+SwaggerExpress.create(swaggerConfig, (err, swaggerExpress) => {
   if (err) {
     throw err;
   }
 
-  // install middleware
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  io.on("connection", onConnection(database));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  const swaggerDocument = YAML.load("./api/swagger/swagger.yaml");
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  app.use((_, res, next) => {
+    res.database = database;
+    res.mail = sendmail;
+    next();
+  });
+
+  app.use(logger("dev"));
+
   swaggerExpress.register(app);
+
+  app.use((err, req, res, next) => {
+    try {
+      const { code, message, path } = err.results.errors[0];
+      const error = `${code} for "${path.join("->")}": ${message}`;
+      res.status(500).json({ error });
+    } catch (_) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 
   var port = 3001;
 
@@ -67,31 +79,31 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   });
 });
 
+// WEB SOCKET
+
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-
 const { getSessions } = require("./helpers/firebaseSession.helpers");
-
 const { findKey } = require("lodash");
 
-const onConnection = async socket => {
+const onConnection = db => async socket => {
   const date = new Date().getTime();
   const { token } = socket.handshake.query;
   console.log("SOKETIO, a user connected");
-  const sessions = await getSessions(database);
+  const sessions = await getSessions(db);
 
   const id = findKey(sessions, sessionToken => sessionToken === token);
   if (id) {
     console.log("New user connected", id);
-    database.ref("playlists").on("child_removed", () => {
+    db.ref("playlists").on("child_removed", () => {
       socket.emit("GET_PLAYLIST", {
         type: "GET_PLAYLIST",
         data: {}
       });
     });
 
-    database.ref("playlists").on("child_added", snapshot => {
+    db.ref("playlists").on("child_added", snapshot => {
       if (snapshot.val().createdAt > date) {
         socket.emit("GET_PLAYLIST", {
           type: "GET_PLAYLIST",
@@ -100,7 +112,7 @@ const onConnection = async socket => {
       }
     });
 
-    database.ref("playlists").on("child_changed", snapshot => {
+    db.ref("playlists").on("child_changed", snapshot => {
       socket.emit("UPDATED_PLAYLIST", {
         type: "UPDATED_PLAYLIST",
         data: { id: snapshot.val()._id }
@@ -110,5 +122,3 @@ const onConnection = async socket => {
     console.log("New user connected not recognized");
   }
 };
-
-io.on("connection", onConnection);
